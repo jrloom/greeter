a3f476bdb31e30d2c98e218a503b9bc7eba1dbe9
Jason Loomis <jason@jrloomis.com>
2019-10-31 13:22:23 -0400

    dynamic port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const server = express();
+const port = process.env.PORT || 4000;
 
 server.use(express.json());
 
 server.get("/", (req, res) => res.status(200).json({ message: "Good things" }));
 
-server.listen(4000, () => console.log(`\nServer listening on port 4000\n`));
+server.listen(port, () => console.log(`\nServer listening on port ${port}\n`));
